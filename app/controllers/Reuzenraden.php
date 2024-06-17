c2d35570dca060fc13be53b3bcf230e2e7c9b20b
Arjan de Ruijter <rra@mboutrecht.nl>
2024-06-17 14:19:41 +0200

    Initial commit -m

---
+++ app/controllers/Reuzenraden.php
@@ -0,0 +1,37 @@
+<?php
+
+class Reuzenraden extends BaseController
+{
+    private $reuzenradModel;
+
+    public function __construct()
+    {
+        $this->reuzenradModel = $this->model('Reuzenrad');
+    }
+
+    public function index()
+    {
+        $reuzenraden = $this->reuzenradModel->getReuzenraden();
+        
+        // var_dump($zangeressen);exit();
+
+        $dataRows = "";
+
+        foreach ($reuzenraden as $reuzenrad) {
+            $dataRows .= "<tr>
+                            <td>{$reuzenrad->Naam}</td>
+                            <td>{$reuzenrad->Hoogte}</td>
+                            <td>{$reuzenrad->Land}</td>
+                            <td>{$reuzenrad->Kosten}</td>
+                            <td>{$reuzenrad->AantalPassagiers}</td>          
+                        </tr>";
+        }
+
+        $data = [
+            'title' => 'De Top 5 hoogste reuzenraden ter wereld',
+            'dataRows' => $dataRows
+        ];
+
+        $this->view('reuzenraden/index', $data);
+    }
+}
---
+++ app/models/Reuzenrad.php
@@ -0,0 +1,50 @@
+<?php
+
+class Reuzenrad
+{
+    private $db;
+
+    public function __construct()
+    {
+        /**
+         * Maak een nieuw database object die verbinding maakt met de 
+         * MySQL server
+         */
+        $this->db = new Database();
+    }
+
+    /**
+     * Haal alle records op uit de Country-tabel
+     */
+    public function getReuzenraden()
+    {
+        try {
+            /**
+             * Maak een sql-query die de gewenste informatie opvraagt uit de database
+             */
+            $sql = 'SELECT Id
+                          ,Naam
+                          ,Hoogte
+                          ,Land
+                          ,Kosten
+                          ,AantalPassagiers
+                    FROM   Reuzenrad
+                    ORDER BY Hoogte DESC';
+
+            /**
+             * Prepare de query voor het PDO object
+             */
+            $this->db->query($sql);
+
+            /**
+             * Geef de opgehaalde informatie terug aan de controller
+             */
+            return $this->db->resultSet();
+            
+        } catch (Exception $e) {
+            // Behandel de uitzondering hier, bijvoorbeeld loggen of een foutmelding weergeven
+            echo 'Er is een fout opgetreden: ' . $e->getMessage();
+        }
+    }
+
+}
